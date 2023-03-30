e5aa63af4326c5f1bbd851e814de4fa90330b97e
Alizéa Massé <alizea777@hotmail.fr>
2023-03-30 12:40:46 +0200

    set up the project

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBPwBmxzERyCAFb6B8pLo5k0xWuqRuWp6U",
+
+  authDomain: "slack-copy-ed14a.firebaseapp.com",
+
+  projectId: "slack-copy-ed14a",
+
+  storageBucket: "slack-copy-ed14a.appspot.com",
+
+  messagingSenderId: "968711250392",
+
+  appId: "1:968711250392:web:70a3d491ae0f8180f852ad"
+
+};
